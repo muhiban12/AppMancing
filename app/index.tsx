90ecb926bee1c513a0d9f2fd5438369d532ab01d
muhiban12 <muhiban12@upi.edu>
2026-01-07 10:07:40 +0700

    __

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 // app/index.tsx
 import React from 'react';
-import SplashScreen from '../splash'; // pastikan file ini ada di app/splashscreen.tsx
+import SplashScreen from './splash'; // pastikan file ini ada di app/splashscreen.tsx
 
 export default function Index() {
   return <SplashScreen />;
